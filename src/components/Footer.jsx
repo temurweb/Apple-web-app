8655becc8927b04fcf8a2d1c01a27688050b8e80
Temur Rashidov <rashidov27@outlook.com>
2026-03-17 19:21:30 +0500

    footer responsive updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      <div className='bg-gray-200 text-gray-600 py-10 px-20 flex justify-center text-[12px]'>
+      <div className='bg-gray-200 text-gray-600 py-5 md:py-10 px-5 md:px-20 flex justify-center text-[12px]'>
        <div className='flex flex-col items-center'>
         <p>
         1. Trade‑in values will vary based on the condition, year, and configuration of your eligible trade‑in device. Not all devices are eligible for credit. You must be at least the age of majority to be eligible to trade in for credit or for an Apple Gift Card. Trade‑in value may be applied toward qualifying new device purchase, or added to an Apple Gift Card. Actual value awarded is based on receipt of a qualifying device matching the description provided when estimate was made. Sales tax may be assessed on full value of a new device purchase. In‑store trade‑in requires presentation of a valid photo ID (local law may require saving this information). Offer may not be available in all stores and may vary between in‑store and online trade‑in. Some stores may have additional requirements. Apple or its trade‑in partners reserve the right to refuse, cancel, or limit quantity of any trade‑in transaction for any reason. More details are available from Apple’s trade-in partner for trade‑in and recycling of eligible devices. Restrictions and limitations may apply. 
