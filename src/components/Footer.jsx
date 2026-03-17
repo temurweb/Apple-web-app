7ea2c92e71f0c84090e1d7d53a56ae676149dd8b
Temur Rashidov <rashidov27@outlook.com>
2026-03-17 19:13:17 +0500

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,18 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      <div className='bg-gray-200 '>
-       <p>
-        I made this web-app to learn design style of Apple company. This website has some features such as language switcher, sidebar menu. 
+      <div className='bg-gray-200 text-gray-600 py-10 px-20 flex justify-center text-[12px]'>
+       <div className='flex flex-col items-center'>
+        <p>
+        1. Trade‑in values will vary based on the condition, year, and configuration of your eligible trade‑in device. Not all devices are eligible for credit. You must be at least the age of majority to be eligible to trade in for credit or for an Apple Gift Card. Trade‑in value may be applied toward qualifying new device purchase, or added to an Apple Gift Card. Actual value awarded is based on receipt of a qualifying device matching the description provided when estimate was made. Sales tax may be assessed on full value of a new device purchase. In‑store trade‑in requires presentation of a valid photo ID (local law may require saving this information). Offer may not be available in all stores and may vary between in‑store and online trade‑in. Some stores may have additional requirements. Apple or its trade‑in partners reserve the right to refuse, cancel, or limit quantity of any trade‑in transaction for any reason. More details are available from Apple’s trade-in partner for trade‑in and recycling of eligible devices. Restrictions and limitations may apply. 
        </p>
+       <p className='mt-5'>
+        To access and use all Apple Card features and products available only to Apple Card users, you must add Apple Card to Wallet on an iPhone or iPad that supports and has the latest version of iOS or iPadOS. Apple Card is subject to credit approval, available only for qualifying applicants in the United States, and issued by Goldman Sachs Bank USA, Salt Lake City Branch.
+       </p>
+       <p className='mt-5'>
+        Apple Payments Services LLC, a subsidiary of Apple Inc., is a service provider of Goldman Sachs Bank USA for Apple Card and Savings accounts. Neither Apple Inc. nor Apple Payments Services LLC is a bank.
+       </p>
+       </div>
       </div>
     </div>
   )
